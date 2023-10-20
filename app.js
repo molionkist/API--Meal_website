d64ad27ab549163886cbfb8d236546e9e3fb985f
Fahim <istiakfahim94@gmail.com>
2023-10-20 18:21:41 +0600

    click to display data

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const displaySearchResult = meals => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
+        <div onclick="loadMealDetail('${meal.idMeal}')" class="card">
                 <img src="${meal.strMealThumb}">
     
                     <h5 class="card-title">${meal.strMeal}</h5>
@@ -26,6 +26,30 @@ const displaySearchResult = meals => {
             </div > `;
 
         searchResult.appendChild(div);
-    })
+    });
+}
+const loadMealDetail = mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0]))
+}
+const displayMealDetail = meal => {
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.innerHTML = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">${meal.strInstructions}</p>
+            </div>;`
+
+    mealDetails.appendChild(div);
+
 
 }
+
+
+
